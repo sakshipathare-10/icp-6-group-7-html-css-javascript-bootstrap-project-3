9910fa9c706e85bf9854d0a72d7a1c856d38c8af
sakshi pathare <sakshipathare304@gmail.com>
2024-01-14 10:44:23 +0530

    code added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function login() {
+    let username = document.getElementById("username").value;
+     
+    // Your login validation logic here
+    if (username == "admin")
+     {
+      alert("response successfully submitted!");
+    }
+     
+  }
